6c3d18192331f950c24a18861a5885dff1945157
Himesh satyam <satyamhimesh@gmail.com>
2023-02-01 16:37:21 +0530

    commited new

---
+++ FirstClass.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class FirstClass{
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine(); 
+        System.out.println(name);  
+        // a
+    }
+}
