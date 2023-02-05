20d77825ae678d79e71c4160824b41bbc9ac09d7
Himesh satyam <satyamhimesh@gmail.com>
2023-02-05 20:16:19 +0530

    condition

--- FirstClass.java
+++ FirstClass.java
@@ -3,12 +3,10 @@ class FirstClass{
     public static void main(String[] args) {
         //Input
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter name : ");
-        String name = sc.nextLine();
-        System.out.println("Enter age : ");
-        int k = sc.nextInt(); 
-        System.out.println("Name : "+name);  
-        System.out.println("Age : "+k);
-        // a
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("sum :"+sum);
+        
     }
 }
