b9f69105cc178b1f9d07587af115106d7a1a146c
Himesh satyam <satyamhimesh@gmail.com>
2023-02-15 13:23:25 +0530

    patterns

---
+++ binpattern.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class binpattern{
+    public static void main(String[] args) {
+        int n=4;
+      /*   for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                int sum = i+j;
+                if(sum%2==0){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();    */
+
+      
+        //upper part of butterfly
+             
+    }
+}
